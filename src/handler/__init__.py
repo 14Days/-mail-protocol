af0223747bc23407ea6e1e41432e105cb1c2116e
wangjq4214 <w1637894214@163.com>
2020-04-23 20:34:55 +0800

    添加部署文件

--- src/handler/__init__.py
+++ src/handler/__init__.py
@@ -5,10 +5,10 @@ from src.lib.pop_server.controller import Controller as PController
 
 
 async def smtp_main(loop):
-    cont = SController(SaveMail(), hostname='::0', port=8025)
+    cont = SController(SaveMail(), hostname='0.0.0.0', port=8025)
     cont.start()
 
 
 async def pop3_main(loop):
-    cont = PController(SendMail(), hostname='::0', port=8026)
+    cont = PController(SendMail(), hostname='0.0.0.0', port=8026)
     cont.start()
